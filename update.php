e679707d892f251737d1f54f39b2ff2d6fa4bf8e
Edrard A <demonand@gmail.com>
2019-12-10 17:16:15 +0200

    Create update.php
    Updater

---
+++ update.php
@@ -0,0 +1,8 @@
+<?php
+
+
+exec("git checkout origin/master src");
+exec("git checkout origin/master backup.sh");
+exec("git checkout origin/master composer.json");
+exec("git checkout origin/master run_sync.php");
+exec("composer update");
