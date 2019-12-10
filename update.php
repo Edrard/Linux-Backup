3a7d8dca2420b4bcb129b3a14b68d70bda01b21a
Edrard A <demonand@gmail.com>
2019-12-10 17:42:12 +0200

    Update
    some fixes

--- update.php
+++ update.php
@@ -5,4 +5,5 @@ exec("git checkout origin/master src");
 exec("git checkout origin/master backup.sh");
 exec("git checkout origin/master composer.json");
 exec("git checkout origin/master run_sync.php");
+exec("git checkout origin/master update.php");
 exec("composer update");
