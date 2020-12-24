9d616d21890598323f2b72e8491ea77ef629b38b
Edrard A <demonand@gmail.com>
2020-12-24 14:21:20 +0200

    Update update.php

--- update.php
+++ update.php
@@ -6,10 +6,9 @@ require __DIR__ . '/vendor/autoload.php';
 
 $string = file_get_contents("ftp.json");
 
-exec("git stash save \"Changes I don't want to commit yet\" ");
+exec("git fetch --all");
+exec("git reset --hard origin/master");
 exec("git pull");
-exec("git stash pop");
-
 exec("composer update");
 
 if($string && is_json($string)){
