ce7d895806e7bcffdefbf4fc1bd6c9f86599aad3
Edrard A <demonand@gmail.com>
2020-12-24 14:03:33 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,9 +1,12 @@
 <?php
 
+$string = file_get_contents("ftp.json");
 
-exec("git checkout origin/master src");
-exec("git checkout origin/master backup.sh");
-exec("git checkout origin/master composer.json");
-exec("git checkout origin/master run_sync.php");
-exec("git checkout origin/master update.php");
+exec("git pull");
 exec("composer update");
+
+if($string && is_json($string)){
+    $myfile = fopen("ftp.json", "w") or die("Unable to open file!");
+    fwrite($myfile, $string);
+    fclose($myfile);
+}
